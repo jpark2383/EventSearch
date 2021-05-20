d510887f60d47cabcf6c6b97ae55915efa0993f1
Jon Park <jonathan.park@drivedrift.com>
2021-05-20 12:59:16 -0500

    Initial Commit

---
+++ EventSearchTests/EventSearchTests.swift
@@ -0,0 +1,33 @@
+//
+//  EventSearchTests.swift
+//  EventSearchTests
+//
+//  Created by Jonathan Park on 5/20/21.
+//
+
+import XCTest
+@testable import EventSearch
+
+class EventSearchTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
